45791f73e9b1c6b675288fa09cbf4b3a122979ee
Joel Conalty <joelconalty@gmail.com>
2023-09-05 10:03:45 +0100

    Typewriter effect on AI text field

--- script/script.js
+++ script/script.js
@@ -1,34 +1,53 @@
-
+const Player = (sign) => {
+	this.sign = sign;
+	const getSign = () => sign;
+	return { getSign }
+}
 // IIFE to set up the board and related functions
 const gameBoard = (() => {
-
 	let board = ['', '', '',
 							 '', '', '',
 							 '', '', ''];
 
-	const setGridArea = (index, sign) => {
+	const setSquare = (index, sign) => {
 		board[index] = sign;
 	}
 
-	const getGridArea = (index) => {
+	const getSquare = (index) => {
 		return board[index];
 	}
 
-	const reset = () => {
+	const resetBoard = () => {
 		for(let i = 0; i < board.length; i++) {
 			board[i] = '';
 		}
 	}
 
-	return { setGridArea, getGridArea, reset }
+	return { setSquare, getSquare, resetBoard }
 })();
 
+// IIFE to set up display-related functions
 const displayController = (() => {
-	const gridAreas = Array.from(document.getElementsByClassName('grid-area'))
-	console.log(gridAreas)
-	gridAreas.forEach(area => {
-		area.addEventListener('click', e => {
-			console.log(e.target.id)
+	const squares = Array.from(document.getElementsByClassName('square'))
+	console.log(squares)
+	squares.forEach(square => {
+		square.addEventListener('click', e => {
+			// IF PLAYER's TURN THEN
+			// PLAY ROUND
 		})
 	})
+
+	const updateGameBoard = () => {
+		// ITERATE THRU board ARRAY
+		// CHANGE HTML TO DISPLAY IT
+	}
+})();
+
+// IIFE to set up game-state-related functions
+const gameController = (() => {
+	const playRound = (index) => {
+		// SET GIVEN INDEX TO X OR O
+		// CHECK FOR WINS
+		// CHECK FOR ROUND 9
+	}
 })();
